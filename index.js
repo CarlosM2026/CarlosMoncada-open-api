8055469a5413e62bd5f8912c58444d7877f6cacc
CarlosM2026 <carlosmoncada2022@gmail.com>
2024-05-24 21:50:35 -0400

    commit 3, different API for location + HTML things

--- index.js
+++ index.js
@@ -6,12 +6,24 @@ function getInfo(event) {
 
     const Weathersection = document.querySelector("#list_for_temp")
 
+    const CityHeader = document.querySelector("#nearest_city")
+
     const ul_for_temps = Weathersection.querySelector("ul")
 
     ul_for_temps.innerHTML = '';
+    CityHeader.innerText = '';
 
     let latitude = event.target.lattitude.value
     let longtitude = event.target.longtitude.value
+
+    fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${latitude}&lon=${longtitude}&apiKey=a1cd90b524be46759041571fdeea33cf`)
+    .then(function(response) {
+        return response.json()
+    }) .then(function(response) {
+        CityHeader.innerText = response.features[0].properties.formatted;
+    }) .catch(function(error) {
+        CityHeader.innerText = "Could not find city"; 
+    })
 
     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longtitude}&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,weather_code`) //https://api.open-meteo.com/v1/forecast?latitude=45.0&longitude=-93.0&current_weather=true&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m,precipitation,weather_code
 
@@ -33,6 +45,9 @@ function getInfo(event) {
 
     }) .catch(function(error) {
         console.log(error)
+        let Temp = document.createElement("li")
+        Temp.innerText = "An error occurred";
+        ul_for_temps.appendChild(Temp)
     })
 
     messageForm.reset()
